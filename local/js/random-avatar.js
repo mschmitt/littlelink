8c6a28070fb1f1ff2ab0733dc8597f83b2584dce
Martin Schmitt <mas@scsy.de>
2024-04-14 13:08:36 +0000

    Minor math fix

--- local/js/random-avatar.js
+++ local/js/random-avatar.js
@@ -3,7 +3,7 @@ $(document).ready(display_random_avatar);
 function display_random_avatar(){
     var avatars = $("img.avatar").length;
     var ava_idx = avatars - 1;    
-    var random = Math.floor(Math.random() * ava_idx);
-    // console.log(avatars + ' avatars to choose from, max index ' + ava_idx + ', random index ' + random + ', thanks for playing.')
+    var random = Math.floor(Math.random() * avatars);
+    console.log(avatars + ' avatars to choose from, max index ' + ava_idx + ', random index ' + random + ', thanks for playing.')
     $("img.avatar").eq(random).css({"display":"inline"});
 }
