2f5f194321f959f3f876b86bdeefa1e54ec4c9db
Martin Schmitt 🐧 <mas@scsy.de>
2024-06-04 17:59:21 +0200

    New deltachat onboarding procedure

--- local/js/local.js
+++ local/js/local.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-	$('#deltachat-dialog').dialog({
-		autoOpen: false,
-		modal: true,
-		width: 300,
-	});
-	$('.button-deltachat').click(function(event){
-		event.preventDefault();
-		$('#deltachat-dialog').dialog('open');
-	});
 	$('#whatsapp-dialog').dialog({
 		autoOpen: false,
 		modal: true,
