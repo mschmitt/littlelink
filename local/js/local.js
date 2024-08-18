10a44751d4e773cffaa1410e18ce8b73320965a4
Martin Schmitt 🐧 <mas@scsy.de>
2024-08-18 11:41:46 +0200

    Update mailto

--- local/js/local.js
+++ local/js/local.js
@@ -2,5 +2,5 @@
 $(document).ready(function(){
 	$('#jshint').css('display', 'none');
 	$('#mail').text(atob('bWl0Y2hAdW5yZWlmLmV1'));
-	$('.button-mail').attr('href', atob('bWFpbHRvOm1hc0BzY3N5LmRlCg=='));
+	$('.button-mail').attr('href', atob('bWFpbHRvOm1pdGNoQHVucmVpZi5ldQ=='));
 });
