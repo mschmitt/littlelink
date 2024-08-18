a9bcf7a37328ede0669c54df01bbf6c9df2a1dcd
Martin Schmitt 🐧 <mas@scsy.de>
2024-08-18 11:37:14 +0200

    Initial work on mail button

---
+++ local/js/local.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+	$('#jshint').css('display', 'none');
+	$('#mail').text(atob('bWl0Y2hAdW5yZWlmLmV1'));
+	$('.button-mail').attr('href', atob('bWFpbHRvOm1hc0BzY3N5LmRlCg=='));
+});
