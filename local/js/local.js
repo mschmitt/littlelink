356a3a51d9abb7bb45f5f2f58b52819a6869696e
Martin Schmitt 🐧 <mas@scsy.de>
2025-02-01 10:56:06 +0100

    Remove Mail and Threema

--- local/js/local.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function(){
-	$('#jshint').css('display', 'none');
-	$('#mail').text(atob('bWl0Y2hAdW5yZWlmLmV1'));
-	$('.button-mail').attr('href', atob('bWFpbHRvOm1pdGNoQHVucmVpZi5ldQ=='));
-});
